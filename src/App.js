cb530e9b0644c04a554beaba061425982d8f016a
ahargunidra <ardhyfals1961@gmail.com>
2021-01-24 15:22:24 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/HeaderMenu"
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar />
+            <Header />
+        </div>
+    );
+}
+
+export default App;
